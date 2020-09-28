3ccaa70c5a24157cf979248b1578a934dad412f3
Murimaral <murimaral@gmail.com>
2020-09-28 19:24:15 -0300

    corrigido o model para user
    Co-authored-by: Alexandre<alexandre.alpe@gmail.com>
    Co-authored-by: Eduardo<gris.eduardo@gmail.com>
    Co-authored-by: Luisa<luludila@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_200331) do
+ActiveRecord::Schema.define(version: 2020_09_28_221008) do
 
   create_table "companies", force: :cascade do |t|
     t.string "cnpj"
@@ -28,18 +28,17 @@ ActiveRecord::Schema.define(version: 2020_09_28_200331) do
     t.index ["company_id"], name: "index_customer_cpfs_on_company_id"
   end
 
-  create_table "models", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "user"
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "customer_cpfs", "companies"
