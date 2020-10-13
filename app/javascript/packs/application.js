bdff432aae13d8444cdda4a29bd25027363d14c1
Henrique Morato <riquemorato@gmail.com>
2020-10-13 13:31:42 -0300

    Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("channels")
 
+require('bootstrap')
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
